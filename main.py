647ab6cc55da093fe26c500cb314a6c91a953dbf
IgnacioRisso <gamerdream406@gmail.com>
2023-04-07 03:57:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import tkinter as tk
+import time
 
 root = tk.Tk()
 root.geometry("400x250")
 root.title("Digital Clock")
 root.tk_setPalette("black")
+
+def update_time():
+    hours = time.strftime("%H")
+    print(hours)
+
+    root.after(1000, update_time)
 
 clock_frame = tk.Frame(root)
 
@@ -26,6 +33,20 @@ sun_day_lb.pack(side=tk.LEFT,padx=10)
 
 days_frame.pack(pady=10)
 
+time_lb = tk.Label(clock_frame, font=("DS-Digital",50))
+time_lb.pack(pady=10)
+
+date_frame = tk.Frame(clock_frame)
+
+date_lb = tk.Label(date_frame, font=("DS-Digital",15))
+date_lb.pack(side=tk.LEFT)
+
+am_pm_lb = tk.Label(date_frame, font=("DS-Digital",15))
+am_pm_lb.pack(side=tk.RIGHT)
+
+date_frame.pack(pady=10, fill=tk.X)
+
 clock_frame.pack()
 
+update_time()
 root.mainloop()
