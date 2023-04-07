a9dfc41151099c1cdbe5809c2af8cc1deb2ec9c6
IgnacioRisso <gamerdream406@gmail.com>
2023-04-07 04:30:00 -0300

    Updates

--- main.py
+++ main.py
@@ -6,9 +6,35 @@ root.geometry("400x250")
 root.title("Digital Clock")
 root.tk_setPalette("black")
 
+def clear_days():
+    mon_day_lb.config(fg="#c3c3c3")
+    tue_day_lb.config(fg="#c3c3c3")
+    wed_day_lb.config(fg="#c3c3c3")
+    thu_day_lb.config(fg="#c3c3c3")
+    fri_day_lb.config(fg="#c3c3c3")
+    sat_day_lb.config(fg="#c3c3c3")
+    sun_day_lb.config(fg="#c3c3c3")
+
 def update_time():
     hours = time.strftime("%H")
-    print(hours)
+    minutes = time.strftime("%M")
+    seconds = time.strftime("%S")
+    
+    time_lb.config(text=f"{hours}:{minutes}:{seconds}")
+
+    date = time.strftime("%d")
+    month = time.strftime("%m")
+    year = time.strftime("%Y")
+
+    am_pm = time.strftime("%p")
+
+    date_lb.config(text=f"{date}-{month}-{year}")
+    am_pm_lb.config(text=am_pm)
+
+    day = time.strftime("%a")
+
+    clear_days()
+    days[day].config(fg="white",font=("bold",12))
 
     root.after(1000, update_time)
 
@@ -32,6 +58,14 @@ sun_day_lb = tk.Label(days_frame, text="SUN")
 sun_day_lb.pack(side=tk.LEFT,padx=10)
 
 days_frame.pack(pady=10)
+
+days = {"Mon": mon_day_lb,
+        "Tue": tue_day_lb,
+        "Wed": wed_day_lb,
+        "Thu": thu_day_lb,
+        "Fri": fri_day_lb,
+        "Sat": sat_day_lb,
+        "Sun": sun_day_lb}
 
 time_lb = tk.Label(clock_frame, font=("DS-Digital",50))
 time_lb.pack(pady=10)
